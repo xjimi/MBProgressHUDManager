265cea887ff1cb4ed5e1b3a4a8bf1adbf623d4db
xjimi <fn5128@gmail.com>
2013-11-23 18:10:37 +0800

    Initial Commit

---
+++ Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by YU-HENG WU on 2013/11/23.
+//  Copyright (c) 2013年 xjimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by YU-HENG WU on 2013/11/23.
+//  Copyright (c) 2013年 xjimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
